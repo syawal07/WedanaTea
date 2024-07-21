c90ac7489c6b259130526fafe70457045b50b091
HanaMarhama <marhamahasana@gmail.com>
2024-07-22 03:51:55 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -67,3 +67,13 @@ window.onclick = (e) => {
   }
 };
 
+
+    function toggleDetails(id) {
+      var details = document.getElementById(id);
+      if (details.style.display === "none" || details.style.display === "") {
+        details.style.display = "block";
+      } else {
+        details.style.display = "none";
+      }
+    }
+ 
